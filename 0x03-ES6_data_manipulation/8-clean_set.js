6265d2d1ad728ef8dc001b2a3e419729ff70ecc0
Marwan Fouz <marwanfouz76@gmail.com>
2024-01-09 20:57:04 +0200

    8. Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+  
+  const cleanedValues = [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join("-");
+  return cleanedValues;
+}
