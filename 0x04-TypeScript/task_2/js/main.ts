2d8d9c2c77ddfcdc6be2b6335192fd8423265455
Marwan Fouz <marwanfouz76@gmail.com>
2024-01-12 20:11:22 +0200

    6. Creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -7,7 +7,7 @@ interface DirectorInterface {
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
-  workDirectorTasks(): string;
+  workTeacherTasks(): string;
 }
 
 class Director implements DirectorInterface {
@@ -29,7 +29,7 @@ class Teacher implements TeacherInterface {
   getCoffeeBreak(): string {
       return 'Cannot have a break';
   }
-  workDirectorTasks(): string {
+  workTeacherTasks(): string {
       return 'Getting to work';
   }
 }
@@ -44,3 +44,17 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+function isDirector(employee: Director | Teacher): boolean {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return (employee as Director).workDirectorTasks();
+  }
+  return (employee as Teacher).workTeacherTasks();
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
